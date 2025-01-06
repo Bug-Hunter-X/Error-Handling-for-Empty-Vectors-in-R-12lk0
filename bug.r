687c9697f29885fb28b53235f4e0dbd8549b4aa0
BugHunter-X <askzenai@gmail.com>
2025-01-06 07:50:04 +0100

    Add bug.r file

---
+++ bug.r
@@ -0,0 +1,6 @@
+```r
+# This code attempts to calculate the mean of a vector, but it will fail if the vector is empty.
+my_vector <- numeric(0)
+mean_value <- mean(my_vector)
+print(mean_value)
+```
